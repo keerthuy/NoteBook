735c7149962d09e5e6b733f9a58d1d4f8fa91a6a
keerthuy <keerththanan109@gmail.com>
2025-07-03 00:59:38 +0530

    secound commit

--- server/index.js
+++ server/index.js
@@ -5,10 +5,9 @@ import authRouter from './routes/auth.js'
 
 const app = express()
 
-
+app.use(express.json())
 app.use(cors())
 app.use('/api/auth',authRouter)
-app.use(express.json())
 
 app.listen(5000,() => {
     connectToMongoDB();
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,14 +1,14 @@
 import express from 'express'
 import User from '../models/user.js';
 import bcrypt from 'bcrypt';
-
+import jwt from 'jsonwebtoken';
 
 const router = express.Router()
 
 router.post('/register',async (req,res) =>{
     try{
 
- const { name,email,password } = req.body;
+ const { name ,email,password } = req.body;
  const user = await User.findOne({email})
  if(user){
     return res.status(401).json({success:false, message:"User already exist"})
@@ -24,7 +24,39 @@ await newUser.save()
 
 return res.status(200).json({success:true , message:"Account Created Successfully"})
     }catch(error){
-return res.status(500).json({success:false , message:"Error Adding User"})
+        console.error(error); // Log the error for debugging
+        return res.status(500).json({success:false , message:"Error Adding User", error: error.message})
+    }
+})
+
+
+
+
+router.post('/login',async (req,res) =>{
+    try{
+
+ const { email,password } = req.body;
+ const user = await User.findOne({email})
+ if(!user){
+    return res.status(401).json({success:false, message:"User Not exist"})
+ }
+
+const checkPassword = await bcrypt.compare(password, user.password)
+
+if(!checkPassword){
+    return res.status(401).json({success: false, message:"Invailid Password"})
+}
+
+const token = jwt.sign({id: user._id},"securetkeyofnoteapp123@#", {expiresIn :"5h"});
+
+return res
+ .status(200)
+ .json({success: true, token, user:{name:user.name}, message: "Login Sucessfully"});
+
+    }catch(error){
+return res
+.status(500)
+.json({success:false , message:"Error in Login"})
     }
 })
 
